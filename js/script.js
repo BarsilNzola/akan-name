fb69d0abb1cc781f12475d1ee538b8ad16997d97
Barsil Nzola <barsiljohn@gmail.com>
2020-05-10 23:04:57 +0300

    added another function to script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ function akanName() {
 	var yY = yYYY.substring(1,3)
 
 	function dayOftheWeek() {
-		
+		var firstBracket = (cC/4-2*cC-1)
+		var secondBracket = (5*yY/4)
+		var thirdBracket = (26*(mM+1))
+		var calculation = (firstBracket+secondBracket+thirdBracket+dD) mod 7
+		var dow = calculation
 	}
+
+	
 	alert("Your date of birth is" + dob + "and gender is" + gender + "Your Akan Name is .....")
 }
