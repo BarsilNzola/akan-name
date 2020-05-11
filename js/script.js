bcb74344e016ba879f9281c92c0589560762c333
Barsil Nzola <barsiljohn@gmail.com>
2020-05-11 09:59:06 +0300

    added control condition on script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function name() {
-	var mM = document.getElementById(mm);
-	var dD = document.getElementById(dd);
-	var yYYY = document.getElementById(yyyy);
-	var gender = document.getElementById(gender)
+function nameAkan() {
+	var mM = document.getElementById('mm');
+	var dD = document.getElementById('dd');
+	var yYYY = document.getElementById('yyyy');
+	var gender = document.getElementById('gender');
 	var cC = yYYY.substring(0,2)
 	var yY = yYYY.substring(1,3)
 
@@ -10,63 +10,40 @@ function name() {
 		var firstBracket = (cC/4-2*cC-1)
 		var secondBracket = (5*yY/4)
 		var thirdBracket = (26*(mM+1))
-		var calculation = (firstBracket+secondBracket+thirdBracket+dD) mod 7
+		var calculation = (firstBracket+secondBracket+thirdBracket+dD) % 7
 		var dow = calculation
 	}
 
 	function akanname() {
-		switch (dayOftheWeek) {
-			case 0:
-			if (gender = male) {
-				akanname = "Kwasi"
-			} else {
-				akanname = "Akosua"
-			}
-
-			case 1:
-			if (gender = male) {
-				akanname = "Kwadwo"
-			} else {
-				akanname = "Adwoa"
-			}
-
-			case 2:
-			if (gender = male) {
-				akanname = "Kwabena"
-			} else {
-				akanname = "Abenaa"
-			}
-
-			case 3:
-			if (gender = male) {
-				akanname = "Kwaku"
-			} else {
-				akanname = "Akua"
-			}
-
-			case 4:
-			if (gender = male) {
-				akanname = "Yaw"
-			} else {
-				akanname = "Yaa"
-			}
-
-			case 5:
-			if (gender = male) {
-				akanname = "Kofi"
-			} else {
-				akanname = "Afua"
-			}
-
-			case 6:
-			if (gender = male) {
-				akanname = "Kwame"
-			} else {
-				akanname = "Ama"
-			}
-		}	
+		if (dow = 0 && gender === male) {
+			akanname = "Kwasi"
+		} else if (dow = 1 && gender === male) {
+			akanname = "Kwadwo"
+		} else if (dow = 2 && gender === male) {
+			akanname = "Kwabena"
+		} else if (dow = 3 && gender === male) {
+			akanname = "Kwaku"
+		} else if (dow = 4 && gender === male) {
+			akanname = "Yaw"
+		} else if (dow = 5 && gender === male) {
+			akanname = "Kofi"
+		} else if (dow = 6 && gender === male) {
+			akanname = "Kwame"	
+		} else if (dow = 0 && gender === female) {
+			akanname = "Akosua"
+		} else if (dow = 1 && gender === female) {
+			akanname = "Adwoa"
+		} else if (dow = 2 && gender === female) {
+			akanname = "Abenaa"
+		} else if (dow = 3 && gender === female) {
+			akanname = "Akua"
+		} else if (dow = 4 && gender === female) {
+			akanname = "Yaa"
+		} else if (dow = 5 && gender === female) {
+			akanname = "Afua"
+		} else if (dow = 6 && gender === female) {
+			akanname = "Ama"
+		}
 	}
-
-
 	alert("Your Akan Name is" + akanname)
 }
