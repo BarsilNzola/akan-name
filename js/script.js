9e45fb9c67d919f1fecff19906012004b44e08a1
Barsil Nzola <barsiljohn@gmail.com>
2020-05-11 03:44:16 +0300

    added a function to script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function akanName() {
 	var mM = document.getElementById(mm);
 	var dD = document.getElementById(dd);
 	var yYYY = document.getElementById(yyyy);
+	var gender = document.getElementById(gender)
 	var cC = yYYY.substring(0,2)
 	var yY = yYYY.substring(1,3)
 
@@ -13,6 +14,59 @@ function akanName() {
 		var dow = calculation
 	}
 
-	
+	function akanname() {
+		switch (dayOftheWeek) {
+			case 0:
+			if (gender = male) {
+				akanname = "Kwasi"
+			} else {
+				akanname = "Akosua"
+			}
+
+			case 1:
+			if (gender = male) {
+				akanname = "Kwadwo"
+			} else {
+				akanname = "Adwoa"
+			}
+
+			case 2:
+			if (gender = male) {
+				akanname = "Kwabena"
+			} else {
+				akanname = "Abenaa"
+			}
+
+			case 3:
+			if (gender = male) {
+				akanname = "Kwaku"
+			} else {
+				akanname = "Akua"
+			}
+
+			case 4:
+			if (gender = male) {
+				akanname = "Yaw"
+			} else {
+				akanname = "Yaa"
+			}
+
+			case 5:
+			if (gender = male) {
+				akanname = "Kofi"
+			} else {
+				akanname = "Afua"
+			}
+
+			case 6:
+			if (gender = male) {
+				akanname = "Kwame"
+			} else {
+				akanname = "Ama"
+			}
+		}	
+	}
+
+
 	alert("Your date of birth is" + dob + "and gender is" + gender + "Your Akan Name is .....")
 }
